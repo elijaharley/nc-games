c14544dc019ce9c86c1b19a3f176b288face6d0c
Elijah Harley <heyelijaharley@gmil.com>
2021-05-25 17:05:14 +0100

    tried to implement individual reviews

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Categories from './components/Categories';
 import Reviews from './components/Reviews';
 import Users from './components/Users';
 import { UserProvider } from './context/user';
-import RequireLogin from './components/RequireLogin';
+//import RequireLogin from './components/RequireLogin';
 
 function App() {
   return (
@@ -15,20 +15,20 @@ function App() {
       <Navbar />
       <Switch>
         <UserProvider>
-          <RequireLogin>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route exact path='/reviews'>
-              <Reviews />
-            </Route>
-            <Route exact path='/categories'>
-              <Categories />
-            </Route>
-            <Route exact path='/users'>
-              <Users />
-            </Route>
-          </RequireLogin>
+          {/* <RequireLogin> */}
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/reviews'>
+            <Reviews />
+          </Route>
+          <Route exact path='/categories'>
+            <Categories />
+          </Route>
+          <Route exact path='/users'>
+            <Users />
+          </Route>
+          {/* </RequireLogin> */}
         </UserProvider>
       </Switch>
     </Router>
