04170ae1a99db93c8aa7c7f874c51e5c193a77d2
Elijah Harley <heyelijaharley@gmil.com>
2021-05-27 21:42:54 +0100

    added votes functionality for reviews votes using optimistic rendering

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Badge, Card, Container, Jumbotron } from 'react-bootstrap';
+import { Card, Container, Jumbotron } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as api from '../Utils/api';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import Votes from '../Utils/Votes';
 
 const Reviews = () => {
   const [reviews, setReviews] = useState('');
@@ -31,29 +32,30 @@ const Reviews = () => {
       {reviews.map((reviews) => {
         return (
           <h2 className='m-3' key={reviews.review_id}>
-            <Link to={`/reviews/${reviews.review_id}`}>
-              <Card id='card' className='h-100 shadow-sm bg-white rounded'>
+            <Card id='card' className='h-100 shadow-sm bg-white rounded'>
+              <Link to={`/reviews/${reviews.review_id}`}>
                 <Card.Img
                   variant='top'
                   src={reviews.review_img_url}
                   alt={reviews.title}
                 />
-                <Card.Body className='d-flex flex-column'>
-                  <div className='d-flex mb-2 justify-content-between'>
+              </Link>
+              <Card.Body className='d-flex flex-column'>
+                <div className='d-flex mb-2 justify-content-between'>
+                  <Link to={`/reviews/${reviews.review_id}`}>
                     <Card.Title className='mb-0 font-weight-bold'>
                       {reviews.title}
                     </Card.Title>
-                  </div>
-                  <Card.Text className='text-secondary'>
-                    {reviews.category}
-                  </Card.Text>
-                  Votes {`${reviews.votes}`}
-                  <h5>
-                    <Badge variant='info'>Add Votes</Badge>
-                  </h5>
-                </Card.Body>
-              </Card>
-            </Link>
+                  </Link>
+                </div>
+                <Card.Text className='text-secondary'>
+                  {reviews.category}
+                </Card.Text>
+                <h5>
+                  <Votes votes={reviews.votes} review_id={reviews.review_id} />
+                </h5>
+              </Card.Body>
+            </Card>
           </h2>
         );
       })}
