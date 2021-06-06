2226f1610a92ab224ad0e6162e440aab30c068d7
Elijah Harley <heyelijaharley@gmil.com>
2021-06-06 20:42:26 +0100

    css gridplay

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Button, Card, CardGroup, Container } from 'react-bootstrap';
+import { Button, Card, CardGroup, Container, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as api from '../Utils/api';
 import { useParams } from 'react-router-dom';
@@ -29,68 +29,74 @@ const Reviews = () => {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <Container fluid className='container-layout justify-content-center'>
-      {params.review_id ? `Review #${params.review_id}` : <h2>Reviews</h2>}
-      <div>
-        <h3>
-          <Button
-            variant='info'
-            onClick={() => {
-              setSortOrder('ASC');
-              setReviews((reviews) => [...reviews].sort());
-            }}
-          >
-            Sort Ascending
-          </Button>
-        </h3>
-      </div>
-      <CardGroup className='card-columns'>
-        {reviews.map((reviews) => {
-          return (
-            <h2 className='m-3' key={reviews.review_id}>
-              <Card
-                style={{ width: '18rem' }}
-                id='card-columns'
-                className='h-100 shadow-sm bg-white rounded'
+    <Container className='container-layout'>
+      <Row>
+        <div className='header-text'>
+          {params.review_id ? `Review #${params.review_id}` : <h2>Reviews</h2>}
+          <div>
+            <h3>
+              <Button
+                variant='info'
+                onClick={() => {
+                  setSortOrder('ASC');
+                  setReviews((reviews) => [...reviews].sort());
+                }}
               >
-                <Link to={`/reviews/${reviews.review_id}`} className='link'>
-                  <Card.Img
-                    variant='top'
-                    src={reviews.review_img_url}
-                    alt={reviews.title}
-                    className='img'
-                  />
-                </Link>
-                <Card.Body className='d-flex flex-column'>
-                  <div className='d-flex mb-2 justify-content-around'>
-                    <Link to={`/reviews/${reviews.review_id}`}>
-                      <Card.Title className='mb-0 font-weight-bold'>
-                        {reviews.title}
-                      </Card.Title>
-                    </Link>
-                  </div>
-                  <Link to={`/categories/${reviews.category}`}>
-                    <Card.Text className='text-secondary'>
-                      {reviews.category
-                        .replaceAll('-', ' ')
-                        .replace(
-                          reviews.category[0],
-                          reviews.category[0].toUpperCase()
-                        )}
-                    </Card.Text>
-                  </Link>
-                  <h3>
-                    <Votes
-                      votes={reviews.votes}
-                      review_id={reviews.review_id}
+                Sort Ascending
+              </Button>
+            </h3>
+          </div>
+        </div>
+      </Row>
+      <Row>
+        <CardGroup className='card-columns'>
+          {reviews.map((reviews) => {
+            return (
+              <h2 className='m-3' key={reviews.review_id}>
+                <Card
+                  style={{ width: '18rem' }}
+                  id='card-columns'
+                  className='h-100 shadow-sm bg-white rounded'
+                >
+                  <Link to={`/reviews/${reviews.review_id}`} className='link'>
+                    <Card.Img
+                      variant='top'
+                      src={reviews.review_img_url}
+                      alt={reviews.title}
+                      className='img'
                     />
-                  </h3>
-                </Card.Body>
-              </Card>
-            </h2>
-          );
-        })}
-      </CardGroup>
+                  </Link>
+                  <Card.Body className='d-flex flex-column'>
+                    <div className='d-flex mb-2 justify-content-around'>
+                      <Link to={`/reviews/${reviews.review_id}`}>
+                        <Card.Title className='mb-0 font-weight-bold'>
+                          {reviews.title}
+                        </Card.Title>
+                      </Link>
+                    </div>
+                    <Link to={`/categories/${reviews.category}`}>
+                      <Card.Text className='text-secondary'>
+                        {reviews.category
+                          .replaceAll('-', ' ')
+                          .replace(
+                            reviews.category[0],
+                            reviews.category[0].toUpperCase()
+                          )}
+                      </Card.Text>
+                    </Link>
+                    <h3>
+                      <Votes
+                        votes={reviews.votes}
+                        review_id={reviews.review_id}
+                      />
+                    </h3>
+                  </Card.Body>
+                </Card>
+              </h2>
+            );
+          })}
+        </CardGroup>
+      </Row>
     </Container>
   );
 };
