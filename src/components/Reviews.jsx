e6335e740afd44fcc9146d1a9ba32a2b6d1db7ee
Elijah Harley <heyelijaharley@gmil.com>
2021-06-08 10:37:23 +0100

    css flex-wrap functionality implemented

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -30,73 +30,69 @@ const Reviews = () => {
 
   return (
     <Container className='container-layout'>
-      <Row>
-        <div className='header-text'>
-          {params.review_id ? `Review #${params.review_id}` : <h2>Reviews</h2>}
-          <div>
-            <h3>
-              <Button
-                variant='info'
-                onClick={() => {
-                  setSortOrder('ASC');
-                  setReviews((reviews) => [...reviews].sort());
-                }}
-              >
-                Sort Ascending
-              </Button>
-            </h3>
-          </div>
-        </div>
-      </Row>
-      <Row>
+      <div className='header-text'>
+        {params.review_id ? `Review #${params.review_id}` : <h2>Reviews</h2>}
+        <h3>
+          <Button
+            variant='info'
+            onClick={() => {
+              setSortOrder('ASC');
+              setReviews((reviews) => [...reviews].sort());
+            }}
+          >
+            Sort Ascending
+          </Button>
+        </h3>
         <CardGroup className='card-columns'>
           {reviews.map((reviews) => {
             return (
               <h2 className='m-3' key={reviews.review_id}>
-                <Card
-                  style={{ width: '18rem' }}
-                  id='card-columns'
-                  className='h-100 shadow-sm bg-white rounded'
-                >
-                  <Link to={`/reviews/${reviews.review_id}`} className='link'>
-                    <Card.Img
-                      variant='top'
-                      src={reviews.review_img_url}
-                      alt={reviews.title}
-                      className='img'
-                    />
-                  </Link>
-                  <Card.Body className='d-flex flex-column'>
-                    <div className='d-flex mb-2 justify-content-around'>
-                      <Link to={`/reviews/${reviews.review_id}`}>
-                        <Card.Title className='mb-0 font-weight-bold'>
-                          {reviews.title}
-                        </Card.Title>
-                      </Link>
-                    </div>
-                    <Link to={`/categories/${reviews.category}`}>
-                      <Card.Text className='text-secondary'>
-                        {reviews.category
-                          .replaceAll('-', ' ')
-                          .replace(
-                            reviews.category[0],
-                            reviews.category[0].toUpperCase()
-                          )}
-                      </Card.Text>
-                    </Link>
-                    <h3>
-                      <Votes
-                        votes={reviews.votes}
-                        review_id={reviews.review_id}
+                <Row>
+                  <Card
+                    style={{ width: '18rem' }}
+                    id='card-columns'
+                    className='h-100 shadow-sm bg-white rounded'
+                  >
+                    <Link to={`/reviews/${reviews.review_id}`} className='link'>
+                      <Card.Img
+                        variant='top'
+                        src={reviews.review_img_url}
+                        alt={reviews.title}
+                        className='img'
                       />
-                    </h3>
-                  </Card.Body>
-                </Card>
+                    </Link>
+                    <Card.Body className='d-flex flex-column'>
+                      <div className='d-flex mb-2 justify-content-around'>
+                        <Link to={`/reviews/${reviews.review_id}`}>
+                          <Card.Title className='mb-0 font-weight-bold'>
+                            {reviews.title}
+                          </Card.Title>
+                        </Link>
+                      </div>
+                      <Link to={`/categories/${reviews.category}`}>
+                        <Card.Text className='text-secondary'>
+                          {reviews.category
+                            .replaceAll('-', ' ')
+                            .replace(
+                              reviews.category[0],
+                              reviews.category[0].toUpperCase()
+                            )}
+                        </Card.Text>
+                      </Link>
+                      <h3>
+                        <Votes
+                          votes={reviews.votes}
+                          review_id={reviews.review_id}
+                        />
+                      </h3>
+                    </Card.Body>
+                  </Card>
+                </Row>
               </h2>
             );
           })}
         </CardGroup>
-      </Row>
+      </div>
     </Container>
   );
 };
